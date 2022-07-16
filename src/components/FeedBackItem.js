3f3effad349c79868c36e483b8ae39f3c63c4a33
mitesh virash <miteshkvirash@gmail.com>
2022-07-16 11:40:02 +0530

    delete func added to the cards

--- src/components/FeedBackItem.js
+++ src/components/FeedBackItem.js
@@ -1,11 +1,15 @@
+import { FaTimes } from "react-icons/fa";
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Card from "./Layout/Card";
 
-const FeedBackItem = ({ item }) => {
+const FeedBackItem = ({ item, deleteFunc }) => {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => deleteFunc(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
--- src/components/FeedBackList.js
+++ src/components/FeedBackList.js
@@ -2,14 +2,14 @@ import React from "react";
 import FeedBackItem from "./FeedBackItem";
 import PropTypes from "prop-types";
 
-const FeedBackList = ({ feedback }) => {
+const FeedBackList = ({ feedback, deleteFunc }) => {
   if (!feedback || feedback.length === 0) {
     return <p>no feedback yet</p>;
   }
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedBackItem key={item.id} item={item} />
+        <FeedBackItem key={item.id} deleteFunc={deleteFunc} item={item} />
       ))}
     </div>
   );
